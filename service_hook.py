a49769d06ecfd61412533771330a77071b79c61b
Igor <55797269+igorlachman@users.noreply.github.com>
2021-02-14 11:38:27 +0200

    Create service_hook.py

---
+++ service_hook.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import os
+
+from flask import Flask, request, json
+
+app = Flask(__name__)
+
+@app.route('/')
+def api_root():
+    return 'API WebHook_GIT'
+
+
+@app.route('/github', methods=['POST'])
+def api_github_mess():
+    if request.headers['Content-Type'] == 'application/json':
+        content = request.json
+        branch_name = content.get('ref')
+        if 'dev_lachman' in branch_name:
+            print('Branch [dev_lachman] updated. Start building a project [simple_flask_app]...')
+            os.system('./testing_project.sh')
+        return json.dumps(request.json)
+
+
+
+if __name__ == '__main__':
+  app.run(host="0.0.0.0", port=5000)
