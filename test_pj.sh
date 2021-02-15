967c6f6b35fe7dc55b7015e0cadcf86d7db6c583
Igor <55797269+igorlachman@users.noreply.github.com>
2021-02-15 14:48:28 +0200

    Update test_pj.sh

--- test_pj.sh
+++ test_pj.sh
@@ -1,39 +1,42 @@
 #!/bin/bash
+  
+APP_ROOT=''
+APP_NAME=''
 
-cd '/home/ubuntu/Flask/'
-rm -rf '/home/ubuntu/Flask/simple_flask_app/'
+IP_ADDR=`ifconfig | grep inet | grep broadcast | grep 255.255.255.0 | awk '{print$2}'`
+BOT_ID=''
+CHANNEL_ID=''
+
+cd $APP_ROOT
+rm -rf $APP_ROOT$APP_NAME
 echo '[Cloning repo] ...'
 echo '[Branch] dev_lachman'
 `git clone --branch dev_lachman https://github.com/igorlachman/simple_flask_app.git`
 echo '[Cloning repo] - [Done]'
 echo '[----------------------------]'
 echo '[Start building an image from Dokerfile] ...'
-cd '/home/ubuntu/Flask/simple_flask_app'
-`sudo /usr/bin/docker build -t simple_flask_app . >> update_log`
-echo '[Build image with lates tag] - [Done]'
+cd $APP_ROOT$APP_NAME
+`/usr/bin/docker build -t simple_flask_app . >> update_log`
+echo '[Build image with latest tag] - [Done]'
 echo '[----------------------------]'
 echo '[Runing container] ...'
-`sudo /usr/bin/docker stop simple_flask_app >> update_log`
-`sudo /usr/bin/docker rm simple_flask_app >> update_log`
-`sudo /usr/bin/docker run --name=simple_flask_app -td -p3000:3000 simple_flask_app >> update_log `
+`/usr/bin/docker stop simple_flask_app >> update_log`
+`/usr/bin/docker rm simple_flask_app >> update_log`
+`/usr/bin/docker run --name=simple_flask_app -td -p3000:3000 simple_flask_app >> update_log `
 echo '[Runing container] - [Done]'
 echo '[----------------------------]'
-STAT=`sudo /usr/bin/docker inspect simple_flask_app|grep 'Status'|awk '{print $2}'`
+STAT=`docker inspect simple_flask_app|grep 'Status'|awk '{print $2}'`
+CONTAINER_ID=`docker ps | grep simple_flask_app | awk '{print$1}'`
+CONTAINER_PORT=`docker ps | grep simple_flask_app | awk '{print$13}'`
 echo '[Container name] - [simple_flask_app]'
+echo '[Container id] - ['$CONTAINER_ID']'
+echo '[Container port] - ['$CONTAINER_PORT']'
 echo '[Status] - ['$STAT']'
 echo '[----------------------------]'
-UPD_BRANCH='Branch|_|dev_lachman|_|update'
-BUILD_STATUS='Build|_|simple_flask_app|_|status|_|complete'
-CONT_STATUS='Container|_|simple_flask_app|_|status|_|'$STAT
-LINK='Container|_|simple_flask_app|_|link|_|http://3.17.78.88:3000/'
-
-`curl --silent 'https://api.telegram.org/<bot_id>/sendMessage?chat_id=<channel_id>&text='$UPD_BRANCH >> update_log`
-`curl --silent 'https://api.telegram.org/<bot_id>/sendMessage?chat_id=<channel_id>&text='$BUILD_STATUS >> update_log`
-`curl --silent 'https://api.telegram.org/<bot_id>/sendMessage?chat_id=<channel_id>&text='$CONT_STATUS >> update_log`
-`curl --silent 'https://api.telegram.org/<bot_id>/sendMessage?chat_id=<channel_id>&text='$LINK >> update_log`
+ST='||Container_ID|_'$CONTAINER_ID'_|Container_status|_'$STAT'_|Container_link|_http://'$IP_ADDR':3000/|_'
+`curl --silent 'https://api.telegram.org/'$BOT_ID'/sendMessage?chat_id='$CHANNEL_ID'&text='$ST >> update_log`
 
 echo '[Bot sended messages to Telegram channel]'
 echo '[Log File] - [/home/ubuntu/Flask/simple_flask_app/update_log]'
 echo '[----------------------------]'
 echo 'All steps done'
-
